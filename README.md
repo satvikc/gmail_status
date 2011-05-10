ea7da75d17c4c85fbb25332242f2b2ad291e6645
satvikc <mystic.satvik@gmail.com>
2011-05-10 22:20:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#Description#
+This is very cool script which you can add to your crontab and see the magic. It will automatically change your status to a randomly chosen quote .
+
+##Requirements##
+python 2.x and fortune-mod.
+
+##Variables##
+
+* USERNAME : You know what it is . Don't add "@gmail.com" .
+* PASSWORD : Your secret word .
+* FORTUNE_COMMAND : Add the categories you want . see Signature [readme](http://github.com/satvikc/Signature) for more details.
+* AVAILABLE : If you want "Red" do not disturb icon with your status  then "dnd" and If you want the "Green" usual icon with your status then "default".
+
+##Usage##
+python status.py
---
+++ status.py
@@ -0,0 +1,23 @@
+import subprocess
+import warnings
+import sys
+from xmpp import *
+FORTUNE_COMMAND="fortune -n 200 -s wisdom"
+USERNAME = "username"       # don't include @gmail.com
+PASSWORD = "password"
+RESOURCE = "gmail.com"
+AVAILABLE = "default"
+def stat():
+    status=subprocess.check_output(FORTUNE_COMMAND.split())[:-1]
+    warnings.filterwarnings("ignore") # silence DeprecationWarning messages
+    cl=Client(server='gmail.com',debug=[])
+    if not cl.connect(server=('talk.google.com',5222)):
+	    raise IOError('Can not connect to server.')
+    if not cl.auth(USERNAME, PASSWORD, RESOURCE):
+        raise IOError('Can not auth with server.')
+    cl.send(Iq('set','google:shared-status',payload=[Node('show',payload=[AVAILABLE]),Node('status',payload=[status])]))
+    cl.disconnect()
+
+if __name__=="__main__":
+    stat()
+

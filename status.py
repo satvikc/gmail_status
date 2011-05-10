fe0be7ba1bf6b277e7348648e1cbf2d7324786ab
satvikc <mystic.satvik@gmail.com>
2011-05-10 23:09:37 +0530

    first commit

--- status.py
+++ status.py
@@ -1,15 +1,13 @@
 import subprocess
-import warnings
 import sys
 from xmpp import *
 FORTUNE_COMMAND="fortune -n 200 -s wisdom"
-USERNAME = "username"       # don't include @gmail.com
+USERNAME = "username"       
 PASSWORD = "password"
 RESOURCE = "gmail.com"
 AVAILABLE = "default"
 def stat():
     status=subprocess.check_output(FORTUNE_COMMAND.split())[:-1]
-    warnings.filterwarnings("ignore") # silence DeprecationWarning messages
     cl=Client(server='gmail.com',debug=[])
     if not cl.connect(server=('talk.google.com',5222)):
 	    raise IOError('Can not connect to server.')
